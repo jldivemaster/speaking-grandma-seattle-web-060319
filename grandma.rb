269d03503ce9b002af8388565cf6b8f77f94dc1f
Jason Leach <jlspursfan@gmail.com>
2019-05-08 23:51:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,10 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(str)
+  if str == str.uppercase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
